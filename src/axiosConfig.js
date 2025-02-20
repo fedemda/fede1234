c317a5754f554f62f53cf9e48a103482a4119eb3
fedemda <fedemda@gmail.com>
2025-02-20 11:50:05 -0300

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -16,17 +16,3 @@ axios.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
-// Manejar errores en las respuestas
-axios.interceptors.response.use(
-  response => response,
-  error => {
-    console.log("❌ Error en respuesta API:", error.response?.status, error.response?.data);
-
-    // Verificar si el token expiró antes de borrar sesión
-    if (error.response?.status === 401) {
-      console.warn("⚠️ Error 401 detectado. NO eliminando sesión automáticamente.");
-    }
-
-    return Promise.reject(error);
-  }
-);
