240b85480a44b7ad98b69daa9228a485a0c21a75
fedemda <fedemda@gmail.com>
2025-02-20 10:43:34 -0300

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
-axios.interceptors.request.use(
-  (config) => {
-    const token = sessionStorage.getItem("token"); // Cambio a sessionStorage
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-      console.log("Token agregado a la solicitud:", token);
-    } else {
-      console.log("No hay token disponible en sessionStorage");
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401 && error.response.data?.message === "Token expirado, inicie sesión nuevamente") {
+      console.warn("⚠️ Token expirado, cerrando sesión...");
+      sessionStorage.clear();
+      window.location.href = "/";
     }
-    return config;
-  },
-  (error) => Promise.reject(error)
+    return Promise.reject(error);
+  }
 );
+
