5475b7d8665a0c745ad5c8c35f5c772a130a0d62
fedemda <fedemda@gmail.com>
2025-02-20 10:30:21 -0300

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 axios.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
+    const token = sessionStorage.getItem("token"); // Cambio a sessionStorage
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
       console.log("Token agregado a la solicitud:", token);
     } else {
-      console.log("No hay token disponible en localStorage");
+      console.log("No hay token disponible en sessionStorage");
     }
     return config;
   },
